7ff8331322d3150d8bb842b05d5e5c4ac45e40f5
Victor Borges <victor.araujo@ccc.ufcg.edu.br>
2018-04-01 15:47:05 -0300

    First function created

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -19,8 +19,12 @@ export class FeedPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  public somaDoisNumeros(): void {
+    alert(5 + 10);
+  }
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+    this.somaDoisNumeros();
   }
 
 }
