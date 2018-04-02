6929d593183c6e084cd6c016dbb01fbddc3ffd66
Victor Borges <victor.araujo@ccc.ufcg.edu.br>
2018-04-02 19:39:40 -0300

    Parameters added to the function

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -19,12 +19,12 @@ export class FeedPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  public somaDoisNumeros(): void {
-    alert(5 + 10);
+  public somaDoisNumeros(num1:number, num2:number): void {
+    alert(num1 + num2);
   }
 
   ionViewDidLoad() {
-    this.somaDoisNumeros();
+    this.somaDoisNumeros(10, 99);
   }
 
 }
